26233d7156a5e3a1524cc8097798e7a8e3a1479b
Sergii Kliaznyka <s.kliaznyka@gmail.com>
2019-06-12 02:49:20 +0300

    Homework 08. Task 02 (Started)

---
+++ KS.UIP.Cpp.HomeWork.HW08.PointerCharTask02/KS.UIP.Cpp.HomeWork.HW08.PointerCharTask02.cpp
@@ -0,0 +1,27 @@
+// KS.UIP.Cpp.HomeWork.HW08.PointerCharTask02.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+//HomeWork 08. Task 02
+//Input sentence. Input method - whatever you like.
+//Input the Letter
+//Show All words, which starts from Input Letter.
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
