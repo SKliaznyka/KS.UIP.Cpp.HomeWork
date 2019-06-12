9d2b43d1c20f24a0031f45e1b2759d13ade4a874
Sergii Kliaznyka <s.kliaznyka@gmail.com>
2019-06-12 22:36:20 +0300

    Homework 08. Task 09

---
+++ KS.UIP.Cpp.HomeWork.HW08.RecursionTask09/KS.UIP.Cpp.HomeWork.HW08.RecursionTask09.cpp
@@ -0,0 +1,28 @@
+// KS.UIP.Cpp.HomeWork.HW08.RecursionTask09.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+//HomeWork 08. Task 09
+//Input the length of square side. Input method - any.
+//Move from (0, 0) to (side, side) position.
+//Move method - Knight move.
+//It is prohibited to make move into the cage where you've been before.
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
