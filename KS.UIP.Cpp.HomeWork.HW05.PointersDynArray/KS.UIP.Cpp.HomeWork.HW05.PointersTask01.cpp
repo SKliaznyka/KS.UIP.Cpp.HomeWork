1396a066c47ccce2ce106779f39e8b6d44b1151a
Sergii Kliaznyka <s.kliaznyka@gmail.com>
2019-09-09 08:36:33 +0300

    HomeWork 05. Changes state 09-09-2019

---
+++ KS.UIP.Cpp.HomeWork.HW05.PointersDynArray/KS.UIP.Cpp.HomeWork.HW05.PointersTask01.cpp
@@ -0,0 +1,42 @@
+// KS.UIP.Cpp.HomeWork.HW05.PointersDynArray.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+//Task 5-(3-1)
+//Given Array. Bunber of elements: 25.
+//Init method - random.
+//Make Stat list:  Number		Number of repetitions
+//Make it with use of Functions.
+//Make it with use of Pointers.
+
+#include "pch.h"
+#include <iostream>
+
+//FUNCTION Init:
+void InitArray(int Array[], int Size);
+void InitArrayPointer(int *Array);
+
+//CONSTS:
+const int iSIZE = 25;
+
+//MAIN:
+int main()
+{
+	int iArray[iSIZE] = { 0 };
+
+
+	return 0; 
+}
+
+//FUNCTION Body:
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
